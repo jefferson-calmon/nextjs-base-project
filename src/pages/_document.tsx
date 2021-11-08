49fb7f0334623e66d68abcea39beb56d5b1ba778
Jefferson Calmon <62778628+jefferson-calmon@users.noreply.github.com>
2021-11-08 15:01:20 -0300

    :rocket:  feat: Add icons and favicon tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -60,9 +60,24 @@ export default class MyDocument extends Document {
                     <meta property="og:image" content="assets/open-graph-image.png" />
 
                     {/* <!-- Icons --> */}
-                    <link rel="icon" href="favicon.ico" />
-                    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-                    <link rel="apple-touch-icon" href="logo.png" />
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
 
                     {/* <!-- Twitter --> */}
                     <meta property="twitter:card" content="assets/open-graph-image.png" />
