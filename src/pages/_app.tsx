93d2081943f043894e547b34154c903dec0f45a2
Jefferson Calmon <jeffjeff0197@gmail.com>
2022-03-09 20:29:51 -0300

    :arrow_up: chore: Upgrade version from `pandora-tools`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,17 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { DefaultSeo } from 'next-seo';
-import pandora from 'pandora-tools';
+import { PandoraConfig } from 'pandora-tools';
 
 import { app } from 'config/app';
 import { light } from 'themes';
 
 import { GlobalStyle } from '../styles/global';
 
-pandora.config()
-
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
 	<ThemeProvider theme={light}>
 		<GlobalStyle />
+        <PandoraConfig />
 
         <DefaultSeo title={app.name} />
 
